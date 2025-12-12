c58b8e8c9322059d02d5ebf8de8e53df852cf0f0
DChiappe <dchiappe@transworld.cl>
2025-12-12 14:57:19 -0300

    xd

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db');
+const { getUsdHoy } = require('../services/usdService');
+
 
 router.get('/', async (req, res) => {
   const { valor: usdHoy } = await getUsdHoy()
