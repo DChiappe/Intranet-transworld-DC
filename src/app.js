4ed30de7eb6f4d562e8cd831fe1b4957f4466238
DChiappe <dchiappe@transworld.cl>
2025-12-17 12:13:11 -0300

    SAD

--- src/app.js
+++ src/app.js
@@ -46,7 +46,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(expressLayouts);
-app.set('layout', 'layout'); // views/layout.ejs
+
 
 
 // ================================
